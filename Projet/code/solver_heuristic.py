76c2d900e43f2c739ec52c1136969845afc406a3
Guillaume Blanché <90753838+GuillBla@users.noreply.github.com>
2023-01-22 12:52:35 -0500

    New version : setting the edges first
    I tried a new heuristic : setting the edges first and then the inner pieces. But it performs less well than the first version :(

--- Projet/code/solver_heuristic.py
+++ Projet/code/solver_heuristic.py
@@ -1,37 +1,7 @@
 import numpy as np
 import math
 import copy
-
-
-def solve_heuristic(eternity_puzzle):
-    """
-    Heuristic solution of the problem
-    :param eternity_puzzle: object describing the input
-    :return: a tuple (solution, cost) where solution is a list of the pieces (rotations applied) and
-        cost is the cost of the solution
-    """
-    solution = []
-
-    remaining_piece = copy.deepcopy(eternity_puzzle.piece_list)
-
-    for i in range(eternity_puzzle.n_piece):
-        best_cost = math.inf
-        
-        for piece in remaining_piece:
-            for rotated_piece in eternity_puzzle.generate_rotation(piece):
-                tested_solution = solution.copy()
-                tested_solution.append(rotated_piece)
-                cost = get_current_cost(tested_solution, eternity_puzzle)
-
-                if cost <= best_cost:
-                    best_cost = cost
-                    best_piece = piece
-                    best_oriented_piece = rotated_piece
-
-        solution.append(best_oriented_piece)
-        remaining_piece.remove(best_piece)
-
-    return (solution, eternity_puzzle.get_total_n_conflict(solution))
+import itertools
 
 
 GRAY = 0
@@ -45,7 +15,137 @@ WEST = 2
 EAST = 3
 
 
-def get_current_cost(solution, eternity_puzzle):
+def solve_heuristic(eternity_puzzle):
+    """
+    Heuristic solution of the problem
+    :param eternity_puzzle: object describing the input
+    :return: a tuple (solution, cost) where solution is a list of the pieces (rotations applied) and
+        cost is the cost of the solution
+    """
+
+    remaining_piece = copy.deepcopy(eternity_puzzle.piece_list)
+
+    solution_grid = [
+        [(0, 0, 0, 0) for _ in range(eternity_puzzle.board_size)]
+        for _ in range(eternity_puzzle.board_size)
+    ]
+
+    edges_and_corners = []
+
+    for piece in eternity_puzzle.piece_list:
+        if GRAY in piece:
+            remaining_piece.remove(piece)
+            edges_and_corners.append(piece)
+
+    # Search for the best layout for edges and corners only
+    for i in range(eternity_puzzle.board_size):
+        for j in range(eternity_puzzle.board_size):
+
+            if (
+                i == 0
+                or j == 0
+                or i == eternity_puzzle.board_size - 1
+                or j == eternity_puzzle.board_size - 1
+            ):
+                print(edges_and_corners)
+
+                best_cost = math.inf
+                for piece in edges_and_corners:
+                    for rotated_piece in eternity_puzzle.generate_rotation(piece):
+                        tested_solution = solution_grid.copy()
+                        tested_solution[i][j] = rotated_piece
+                        cost = get_current_edges_cost(tested_solution, eternity_puzzle)
+
+                        if cost <= best_cost:
+                            best_cost = cost
+                            best_piece = piece
+                            best_oriented_piece = rotated_piece
+
+                solution_grid[i][j] = best_oriented_piece
+                edges_and_corners.remove(best_piece)
+
+    # Search for the best layout with the remaining inner pieces:
+    for i in range(eternity_puzzle.board_size):
+        for j in range(eternity_puzzle.board_size):
+            if (
+                i != 0
+                and j != 0
+                and i != eternity_puzzle.board_size - 1
+                and j != eternity_puzzle.board_size - 1
+            ):
+
+                best_cost = math.inf
+
+                for piece in remaining_piece:
+
+                    for rotated_piece in eternity_puzzle.generate_rotation(piece):
+
+                        tested_solution = solution_grid.copy()
+                        tested_solution[i][j] = rotated_piece
+                        cost = get_inner_cost(tested_solution, eternity_puzzle)
+
+                        if cost <= best_cost:
+                            best_cost = cost
+                            best_piece = piece
+                            best_oriented_piece = rotated_piece
+
+                if len(remaining_piece) > 0:
+                    solution_grid[i][j] = best_oriented_piece
+                    remaining_piece.remove(best_piece)
+
+    # Flatten the grid to a list
+    solution = []
+    for row in solution_grid:
+        for piece in row:
+            solution.append(piece)
+
+    print(eternity_puzzle.piece_list)
+    print(solution)
+
+    return (solution, eternity_puzzle.get_total_n_conflict(solution))
+
+
+def get_current_edges_cost(solution, eternity_puzzle):
+
+    n_conflict = 0
+    for i in range(eternity_puzzle.board_size):
+
+        if solution[0][i][SOUTH] != GRAY:
+            n_conflict += 1
+
+        if solution[eternity_puzzle.board_size - 1][i][NORTH] != GRAY:
+            n_conflict += 1
+
+        if solution[i][eternity_puzzle.board_size - 1][EAST] != GRAY:
+            n_conflict += 1
+
+        if solution[i][0][WEST] != GRAY:
+            n_conflict += 1
+
+        if i > 0 and solution[i][0][SOUTH] != solution[i - 1][0][NORTH]:
+            n_conflict += 1
+
+        if (
+            i > 0
+            and solution[i][eternity_puzzle.board_size - 1][SOUTH]
+            != solution[i - 1][eternity_puzzle.board_size - 1][NORTH]
+        ):
+            n_conflict += 1
+
+        if i > 0 and solution[0][i][WEST] != solution[i - 1][0][EAST]:
+            n_conflict += 1
+
+        if (
+            i > 0
+            and solution[eternity_puzzle.board_size - 1][i][WEST]
+            != solution[eternity_puzzle.board_size - 1][i - 1][EAST]
+        ):
+            n_conflict += 1
+
+    return n_conflict
+
+
+def get_inner_cost(solution, eternity_puzzle):
 
     n_conflict = 0
 
@@ -67,31 +167,16 @@ def get_current_cost(solution, eternity_puzzle):
                 k = eternity_puzzle.board_size * j + i
                 k_west = eternity_puzzle.board_size * j + (i - 1)
                 k_south = eternity_puzzle.board_size * (j - 1) + i
-                print(
-                    "\n solution {} \n n_placed {} \n n_lines {}\n n_columns {}\n i : {} \n j : {} \n k : {} \n k_west {} \n k_south {}".format(
-                        solution, n_placed, n_lines, n_columns, i, j, k, k_west, k_south
-                    )
-                )
-
-                if i == 0 and solution[k][WEST] != GRAY:
-                    n_conflict += 1
-
-                if i == eternity_puzzle.board_size - 1 and solution[k][EAST] != GRAY:
-                    n_conflict += 1
+                # print(
+                #     "\n solution {} \n n_placed {} \n n_lines {}\n n_columns {}\n i : {} \n j : {} \n k : {} \n k_west {} \n k_south {}".format(
+                #         solution, n_placed, n_lines, n_columns, i, j, k, k_west, k_south
+                #     )
+                # )
 
                 if i > 0 and solution[k][WEST] != solution[k_west][EAST]:
                     n_conflict += 1
 
-                if i == eternity_puzzle.board_size - 1 and solution[k][EAST] != GRAY:
-                    n_conflict += 1
-
                 if j > 0 and solution[k][SOUTH] != solution[k_south][NORTH]:
-                    n_conflict += 1
-
-                if j == 0 and solution[k][SOUTH] != GRAY:
-                    n_conflict += 1
-
-                if j == eternity_puzzle.board_size - 1 and solution[k][NORTH] != GRAY:
                     n_conflict += 1
 
     return n_conflict
