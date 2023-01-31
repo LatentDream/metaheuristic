8bd971c91c806ad00647f80ca89ab9a849ca1453
Guillaume <g.thibault@polymtl.ca>
2023-01-31 14:08:21 -0500

    Adding base file for atlas implementation

---
+++ Devoir1/solver_advanced_atlas.py
@@ -0,0 +1,18 @@
+from typing import List, Tuple
+from network import PCSTP
+
+
+def solve(pcstp: PCSTP) -> List[Tuple[int]]:
+    """Advanced solver for the prize-collecting Steiner tree problem.
+
+    Args:
+        pcstp (PCSTP): object containing the graph for the instance to solve
+
+    Returns:
+        solution (List[Tuple[int]]): contains all pairs included in the solution. For example:
+                [(1, 2), (3, 4), (5, 6)]
+            would be a solution where edges (1, 2), (3, 4) and (5, 6) are included and all other edges of the graph 
+            are excluded
+    """
+    # Add here your solving process here
+    raise Exception("Advanced solver not implemented")
